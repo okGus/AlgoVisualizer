e29387aa275b6782bf62bbccaca6cf1be75d7aaa
Gustavo <rosalesagustavo@gmail.com>
2023-12-22 13:33:48 -0500

    Navbar component

---
+++ src/app/components/navigation/navbar/index.tsx
@@ -0,0 +1,23 @@
+import Link from "next/link";
+
+const Navbar = () => {
+    return (
+    <>
+      <div className="w-full h-10 bg-blue-600 sticky top-0">
+        <div className="container mx-auto px-4 h-full">
+          <div className="flex justify-between items-center h-full">
+            <ul className="hidden md:flex gap-x-6 text-white">
+              <li>
+                <Link href="/BubbleSort">
+                  <p>Bubble Sort</p>
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </>
+    );
+};
+
+export default Navbar;
